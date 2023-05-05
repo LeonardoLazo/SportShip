ad3dfcf22913ffdfef9d49f699bdca1e624fb4f3
Jose Leonardo Lazo Hernandez <soft.dev.jose.lazo@gmail.com>
2023-05-04 23:15:08 -0500

    [ MS000001 ] - Maven Project initialization { 001 }

---
+++ src/main/java/com/theorie/SportShip/SportShipApplication.java
@@ -0,0 +1,13 @@
+package com.theorie.SportShip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportShipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportShipApplication.class, args);
+	}
+
+}
